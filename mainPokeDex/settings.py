e09052e2c0c2753e81b1bdc16855ee1d0f8ae480
Spencer Merrill <canadianfaller7@gmail.com>
2022-03-23 20:20:34 -0600

    Fixed bugs so app is working properly now

--- mainPokeDex/settings.py
+++ mainPokeDex/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    Path.joinpath(BASE_DIR, 'pkStatic'),
+    Path.joinpath(BASE_DIR, 'pokeRSC'),
 ]
 
 # Default primary key field type
